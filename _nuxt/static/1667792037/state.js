09f81a82c4e9d78a884672b3d083ca5bd4148f2f
Chau Dang <82007790+dbchau10@users.noreply.github.com>
2022-11-07 10:35:43 +0700

    Add files via upload

---
+++ _nuxt/static/1667792037/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fanotherwebsite\u002F_nuxt\u002Fstatic\u002F1667792037",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fanotherwebsite\u002F",assetsPath:"\u002Fanotherwebsite\u002F_nuxt\u002F",cdnURL:a}}}}(null));
